ee33823ad19e3f03a8f5e31adc5311a974ccff1f
tkastriot <kt30145@seeu.edu.mk>
2024-01-28 16:33:23 +0100

    handle filters in home and cars page

---
+++ RentACar-1/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using RentACar_1.Models;
+
+namespace RentACar_1.ViewModels
+{
+	public class HomeIndexViewModel
+	{
+		public CarFilterViewModel Filters { get; set; }
+		public SelectList BrandList { get; set; }
+		public SelectList CategoryList { get; set; }
+		public SelectList CityList { get; set; }
+	}
+}
